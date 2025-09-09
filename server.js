b219291001a9711aced1c0d8c49a8645814cabe7
Yousaf Nasurullah <yousufjumani2@gmail.com>
2025-09-09 14:33:59 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ const io = new Server(server, {
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL || false 
-    : ["http://localhost:3000", "http://127.0.0.1:3000"],
+    : ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8080", "http://127.0.0.1:8080"],
   credentials: false
 }));
 
